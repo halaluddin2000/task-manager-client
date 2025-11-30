fecebdf31623932824acb33eb883fd622ba77c4b
halaluddin2000 <helaluddin7001@gmail.com>
2025-12-01 03:23:46 +0600

    sing up section added

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
 import { validateEmail } from "../../utils/helper";
 import ProfilePhotoSelector from "../../components/input/ProfilePhotoSelector";
+import { Link } from "react-router-dom";
+import Input from "../../components/input/input";
 
 function SignUp() {
   const [profilePic, setProfilePic] = useState(null);
@@ -34,14 +36,54 @@ function SignUp() {
 
   return (
     <AuthLayout>
-      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col">
+      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
         <h3 className="text-xl font-semibold text-black">Create an Account</h3>
         <p className="text-xs text-slate-700 mt-[5px] mb-6">
           Join us today by entering your details below.
         </p>
         <form onSubmit={handleSignUp}>
           <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4"></div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Input
+              value={fullName}
+              onchange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="John"
+              type="text"
+            />
+            <Input
+              value={email}
+              onchange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="john@example.com"
+              type="text"
+            />
+            <Input
+              value={password}
+              onchange={({ target }) => setPassword(target.value)}
+              label="Password"
+              placeholder="min 8 Characters"
+              type="password"
+            />
+            <Input
+              value={adminInviteToken}
+              onchange={({ target }) => setAdminInviteToken(target.value)}
+              label="Admin Invite Token"
+              placeholder="6 Digit Code"
+              type="text"
+            />
+          </div>
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+          <button type="submit" className="btn-primary">
+            LOGIN
+          </button>
+
+          <p className="text-[13px] text-solid-800 mt-3">
+            Already an account?
+            <Link className="font-medium text-primary underline" to="/login">
+              Login
+            </Link>
+          </p>
         </form>
       </div>
     </AuthLayout>
