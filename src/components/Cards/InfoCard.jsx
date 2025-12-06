9a4c9a9132be87ec585b76ec8a3cca5c916ff11a
halaluddin2000 <helaluddin7001@gmail.com>
2025-12-06 22:01:09 +0600

    task list table

--- src/components/Cards/InfoCard.jsx
+++ src/components/Cards/InfoCard.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-function InfoCard() {
-  return <div></div>;
+function InfoCard({ label, value, color }) {
+  return (
+    <div className="flex items-center gap-3">
+      <div className={`w-5 md:w-2 h-3 md:h-5 ${color} rounded-full`} />
+      <p className="text-xs gap-3 md:text-[14px] text-gray-500">
+        <span className="text-xs md:text-[15px] text-black font-semibold mr-1">
+          {value}
+        </span>
+        {label}
+      </p>
+    </div>
+  );
 }
 
 export default InfoCard;
